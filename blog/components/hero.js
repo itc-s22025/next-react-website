a3370fe3805751699179b2b458bf549dfe0aa862
s22025 <s22025@std.it-college.ac.jp>
2023-01-10 06:24:45 +0000

    seccond commit

--- blog/components/hero.js
+++ blog/components/hero.js
@@ -4,8 +4,8 @@ const Hero = ({ title, subtitle, imageOn = false}) => {
 	return(
 		<div>
 			<div className={styles.text}>
-				<h1>{title}</h1>
-				<p>{subtitle}</p>
+				<h1 className={styles.title}>{title}</h1>
+				<p className={styles.subtitle}>{subtitle}</p>
 			</div>	
 			{imageOn && <figure>[画像]</figure>}
 		</div>
