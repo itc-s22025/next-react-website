49312500dcf88d3c38c1c8c090225cf52c113888
itc-s22025 <104543042+itc-s22025@users.noreply.github.com>
2023-01-12 16:41:05 +0900

    5th finished

--- blog/components/social.js
+++ blog/components/social.js
@@ -8,9 +8,9 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 
 
-const Social = () =>{
+const Social = ({ iconSize = 'initial' }) =>{
 	return (
-		<ul className={styles.list}>
+		<ul className={styles.list} style={{ '--icon-size': iconSize}} >
 			<li>
 				 <a href="https://twitter.com/">
 					<FontAwesomeIcon icon={faTwitter} />
