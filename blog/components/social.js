f39ce7f4f70165f723ba1bd3299d9b025de6b43d
s22025 <s22025@std.it-college.ac.jp>
2023-01-12 07:31:01 +0000

    5th 9 test

--- blog/components/social.js
+++ blog/components/social.js
@@ -3,7 +3,7 @@ import styles from 'styles/social.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
 	faTwitter,
-	faFacebook,
+	faFacebookF,
 	faGithub,
 } from '@fortawesome/free-brands-svg-icons'
 
@@ -12,24 +12,24 @@ const Social = () =>{
 	return (
 		<ul className={styles.list}>
 			<li>
-			 <a href="https://twitter.com/">
-				<FontAwesomeIcon icon={faTwitter} />
-				<span className="sr-only">Twitter</span>
-			 </a>
+				 <a href="https://twitter.com/">
+					<FontAwesomeIcon icon={faTwitter} />
+					<span className="sr-only">Twitter</span>
+				 </a>
 			</li>
 
 			<li>
-			 <a href="https://www.facebook.com/">
-				<FontAwesomeIcon icon={faFacebook} />
-				<span className="sr-only">Facebook</span>
-			 </a>
+				 <a href="https://www.facebook.com/">
+					<FontAwesomeIcon icon={faFacebookF} />
+					<span className="sr-only">Facebook</span>
+				 </a>
 			</li>
 
 			<li>
-			 <a href="https://github.com/">
-				<FontAwesomeIcon icon={faGithub} />
-				<span className="sr-only">Github</span>
-			 </a>
+				 <a href="https://github.com/">
+					<FontAwesomeIcon icon={faGithub} />
+					<span className="sr-only">Github</span>
+				 </a>
 			</li>
 		</ul>
 	)
