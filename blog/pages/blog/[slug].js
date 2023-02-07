d17c4214ec5167464b9bd802730d7716e469f009
s22025 <s22025@std.it-college.ac.jp>
2023-02-07 05:23:55 +0000

    chap9.2 finished

---
+++ blog/pages/blog/[slug].js
@@ -0,0 +1,84 @@
+import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
+import Container from 'components/container'
+//import { client } from 'lib/api'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+
+export default function Post({ title, description, publish, content, eyecatch, categories, }) {
+	return (
+		<Container>
+			<Meta
+				pageTitle={title}
+				pageDesc={description}
+				pageImg={eyecatch.url}
+				pageImgW={eyecatch.width}
+				pageImgH={eyecatch.height}
+			/>
+
+			<article>
+				<PostHeader title={title} subtitle="Blog Article" publish={publish} />
+				<figure>
+					<Image 
+					src={eyecatch.url} 
+					alt="" 
+					layout="responsive" 
+					width={eyecatch.width} 
+					height={eyecatch.height} 
+					sizes="(min-width: 1152px) 1152px, 100vw" 
+					priority
+					plaiceholder="blur"
+					blurDataURL={eyecatch.blurDataURL}
+					/>
+				</figure>
+
+				<TwoColumn>
+					<TwoColumnMain>
+						<PostBody>
+							<ConvertBody contentHTML={content} />
+						</PostBody>
+					</TwoColumnMain>
+					<TwoColumnSidebar>
+						<PostCategories categories={categories} />
+					</TwoColumnSidebar>
+				</TwoColumn>
+			</article>
+		</Container>
+	)
+}
+
+export async function getStaticPaths(){
+	return{
+		paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+		fallback: false,
+	}
+}
+
+export async function getStaticProps(context) {
+	const slug = context.params.slug
+	const post = await getPostBySlug(slug)
+	const description = extractText(post.content)
+	const eyecatch = post.eyecatch ?? eyecatchLocal
+	const { base64 } = await getPlaiceholder(eyecatch.url)
+	eyecatch.blurDataURL = base64
+
+	return{
+		props: {
+			title: post.title,
+			publish: post.publishDate,
+			content: post.content,
+			eyecatch: eyecatch,
+			categories: post.categories,
+			description: description,
+		},
+	}
+}
