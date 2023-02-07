6d43bb4cf4ba8b63197db814cdd4de03f1814af9
s22025 <s22025@std.it-college.ac.jp>
2023-02-07 05:37:26 +0000

    chap9.3 finished

--- blog/pages/blog/[slug].js
+++ blog/pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import { getPostBySlug } from 'lib/api'
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { extractText } from 'lib/extract-text'
 import Meta from 'components/meta'
 import Container from 'components/container'
@@ -57,8 +57,10 @@ export default function Post({ title, description, publish, content, eyecatch, c
 }
 
 export async function getStaticPaths(){
+	const allSlugs = await getAllSlugs()
+
 	return{
-		paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+		paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
 		fallback: false,
 	}
 }
